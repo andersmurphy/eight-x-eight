49e7b10f801dfcab3659adfdbf3c6a39aebce04d
andersmurphy <andersmurphy@gmail.com>
2023-01-20 21:41:52 +0000

    Use preact

--- src/eight_x_eight/app.cljs
+++ src/eight_x_eight/app.cljs
@@ -1,32 +1,37 @@
-(ns eight-x-eight.app
-  (:require
-   ["react-dom/client" :refer [createRoot]]
-   [sablono.core :as sab :include-macros true]))
+(ns eight-x-eight.app)
 
 (def background-music
   (new js/Audio "snds/music.mp3"))
 
-(defn board-screen [_]
-  [[:div.board
-   (concat
-    [[:div.tile {:key 0}
-      [:div.tile__floor]
-      [:div.tile__treasure]]]
-    (for [k (range 1 (inc 64))]
-      (if (even? k)
-        [:div.tile {:key k}
-         [:div.tile__floor]
-         [:div.tile__monster]]
-        [:div.tile {:key k}
-         [:div.tile__wall {:key k}]])))]
-   [:button.nes-btn {:onClick #(do
-                                 (set! background-music -loop true)
-                                 (.play background-music))} "MUSIC"]])
+(def h js/preact.h)
+(defn f [children]
+  (apply h js/preact.Fragment nil children))
 
-(let [container (.getElementById js/document "screen-area")
-      root      (createRoot container)]
+(defn board-screen [_]
+  (f [(h "div" #js {:class "board"}
+         (h "div" #js {:class "tile" :key 0}
+            (h "div" #js {:class "tile__floor"})
+            (h "div" #js {:class "tile__treasure"}))
+         (f (for [k (range 1 (inc 64))]
+              (if (even? k)
+                (h "div" #js {:class "tile" :key k}
+                   (h "div" #js {:class "tile__floor"})
+                   (h "div" #js {:class "tile__monster"}))
+                (h "div" #js {:class "tile" :key k}
+                   (h "div" #js {:class "tile__wall"}))))))
+      (h "button" #js {:class   "nes-btn"
+                       :onclick (fn [_]
+                                  (set! background-music -loop true)
+                                  (.play background-music))}
+         "MUSIC")]))
+
+(let [container (.getElementById js/document "screen-area")]
   (defn renderer [full-state]
-    (.render root (sab/html (board-screen full-state)))))
+    (js/preact.render (board-screen full-state) container)))
+
 (renderer nil)
 
-;; TODO: get repl working (maybe just use cljc for logic
+;; preact supports svg
+;; preact signals seems similar to re-frame (will it work with figwheel?)
+;; need to be able to mix css classes
+;; macro for macking preact easier 
